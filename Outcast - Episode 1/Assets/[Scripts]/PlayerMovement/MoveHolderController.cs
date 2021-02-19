d19ab99c059962e46c7a9c2f918ba85babd4263d
Hvdh Arya <hvdh_ts@live.com>
2021-02-19 19:39:23 +0330

    move holder new

---
+++ Outcast - Episode 1/Assets/[Scripts]/PlayerMovement/MoveHolderController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MoveHolderController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public int buttonCode;
+    public GameObject run;
+    PlayerMovement move;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+        Color holdColor = GetComponent<Image>().color;
+        holdColor.a = 0.5f;
+        GetComponent<Image>().color = holdColor;
+
+        if (buttonCode == 0) //moveLeft
+        {
+            move.MoveLeft();
+            run.SetActive(true);
+        }
+        if (buttonCode == 1) //moveRight
+        {
+            move.MoveRight();
+            run.SetActive(true);
+        }
+        if (buttonCode == 2) //run
+        {
+            move.Run();
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Color holdColor = GetComponent<Image>().color;
+        holdColor.a = 1f;
+        GetComponent<Image>().color = holdColor;
+
+        if(buttonCode == 0 || buttonCode == 1)
+        {
+            run.SetActive(false);
+            move.Stop();
+        }
+
+        if(buttonCode == 2)
+        {
+            move.RunStop();
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        move = FindObjectOfType<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
