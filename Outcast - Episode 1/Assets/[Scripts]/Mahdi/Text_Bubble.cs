0f5979183b57b180aca5b4bdee033d3b5f446fb0
mahdiLotfi2001 <77632254+mahdiLotfi2001@users.noreply.github.com>
2021-06-19 11:10:58 -0700

    Test

---
+++ Outcast - Episode 1/Assets/[Scripts]/Mahdi/Text_Bubble.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text_Bubble : MonoBehaviour
+{
+    PlayerMovement playerMovement;
+    void Awake()
+    {
+        playerMovement = GameObject.FindObjectOfType<PlayerMovement>();
+    }
+
+    private void OnEnable()
+    {
+        playerMovement.RunStop();
+        playerMovement.Stop();
+        playerMovement.enabled = false;
+    }
+
+    private void OnDisable()
+    {
+        playerMovement.enabled = true;
+    }
+}
