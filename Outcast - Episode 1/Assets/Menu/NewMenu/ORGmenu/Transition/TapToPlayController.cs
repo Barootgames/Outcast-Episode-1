7fb0560229df71a8f9892e5246f3666568563a68
Hvdh Arya <hvdh_ts@live.com>
2021-06-24 22:41:59 +0430

    main menu audio

---
+++ Outcast - Episode 1/Assets/Menu/NewMenu/ORGmenu/Transition/TapToPlayController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TapToPlayController : MonoBehaviour
+{
+    public GameObject ttp;
+    public Button ttpWhole;
+    public float ttpDelayTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ttp.SetActive(false);
+        ttpWhole.enabled = false;
+        StartCoroutine(ShowTTP());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator ShowTTP()
+    {
+        yield return new WaitForSeconds(ttpDelayTime);
+        ttp.SetActive(true);
+        ttpWhole.enabled = true;
+    }
+}
---
+++ Outcast - Episode 1/Assets/[Scripts]/AudioManagers/MainMenuAudioController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuAudioController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioClip[] AudioClips; //0 whoosh, 1 back button, 2 click other buttons, 3 new game
+    public AudioSource MusicAudioSource;
+    public AudioSource SFXAudioSource;
+    public AudioSource SFXAudioSource2;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayMenuWhoosh()
+    {
+        PlaySFX(0);
+    }
+
+    public void PlayMenuBackButton()
+    {
+        PlaySFX(1);
+    }
+
+    public void PlayStartStoryClick()
+    {
+        PlaySFX(3);
+        MusicAudioSource.Stop();
+    }
+
+    public void PlayClickSound1()
+    {
+        PlaySFX2(2);
+    }
+
+    void PlaySFX(int clipId)
+    {
+        SFXAudioSource.Stop();
+        SFXAudioSource.clip = AudioClips[clipId];
+        SFXAudioSource.Play();
+    }
+    void PlaySFX2(int clipId)
+    {
+        SFXAudioSource2.Stop();
+        SFXAudioSource2.clip = AudioClips[clipId];
+        SFXAudioSource2.Play();
+    }
+}
